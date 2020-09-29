91861/wayst

commit 7d5d676919f78e29292872420c9eafa118ad2e9e

Clean up vt scroll control + add more options

src/main.c
@@ -25,10 +25,10 @@
 #endif
 
 #include "freetype.h"
+#include "key.h"
 #include "settings.h"
 #include "ui.h"
 #include "vt.h"
-
 
 #ifndef DOUBLE_CLICK_DELAY_MS
 #define DOUBLE_CLICK_DELAY_MS 300
@@ -166,13 +166,17 @@ static void App_init(App* self)
 static void App_run(App* self)
 {
     while (!(self->exit || Window_is_closed(self->win))) {
-        int timeout_ms = self->swap_performed
-                           ? 0
-                           : self->closest_pending_wakeup
-                               ? TimePoint_is_ms_ahead(*(self->closest_pending_wakeup))
-                               : -1;
+        int timeout_ms = 0;
+        if (!self->swap_performed) {
+            if (self->closest_pending_wakeup) {
+                timeout_ms = MAX(0, TimePoint_is_ms_ahead(*(self->closest_pending_wakeup)));
+            } else {
+                timeout_ms = -1;
+            }
+        }
 
         Monitor_wait(&self->monitor, timeout_ms);
+
         self->closest_pending_wakeup = NULL;
         if (Monitor_are_window_system_events_pending(&self->monitor)) {
             Window_events(self->win);
@@ -363,77 +367,81 @@ static bool App_handle_keyboard_select_mode_key(App*     self,
                                                 uint32_t rawkey,
                                                 uint32_t mods)
 {
+    Vt* vt = &self->vt;
+
+#define L_UPDATE_SELECT_END                                                                        \
+    if (self->vt.selection.mode) {                                                                 \
+        size_t col = self->ksm_cursor.col;                                                         \
+        size_t row = self->ksm_cursor.row - Vt_visual_top_line(vt);                                \
+        Vt_select_set_end_cell(vt, col, row);                                                      \
+    }
+
     switch (key) {
         case 27: // Escape
-            Vt_select_end(&self->vt);
+            Vt_select_end(vt);
             App_notify_content_change(self);
             self->keyboard_select_mode = false;
-            Vt_visual_scroll_reset(&self->vt);
+            Vt_visual_scroll_reset(vt);
             Gfx_notify_action(self->gfx);
             return true;
 
-        case 65361: // left
-        case 104:   // h
+        case KEY(Left):
+        case KEY(h):
             App_notify_content_change(self);
             if (self->ksm_cursor.col) {
                 self->ksm_cursor.col--;
             }
-            if (self->vt.selection.mode) {
-                Vt_select_set_end_cell(&self->vt, self->ksm_cursor.col, self->ksm_cursor.row);
-            }
+            L_UPDATE_SELECT_END
             break;
 
-        case 65364: // down
-        case 106:   // j
+        case KEY(Down):
+        case KEY(j):
             App_notify_content_change(self);
-            if (self->ksm_cursor.row < self->vt.lines.size - 1) {
+            if (self->ksm_cursor.row < Vt_max_line(vt)) {
                 self->ksm_cursor.row++;
             }
-            if (Vt_visual_bottom_line(&self->vt) == self->vt.lines.size - 1) {
-                Vt_visual_scroll_reset(&self->vt);
+            if (Vt_visual_bottom_line(vt) == Vt_max_line(vt)) {
+                Vt_visual_scroll_reset(vt);
             } else {
-                while (Vt_visual_bottom_line(&self->vt) < self->ksm_cursor.row) {
-                    Vt_visual_scroll_down(&self->vt);
+                while (Vt_visual_bottom_line(vt) < self->ksm_cursor.row) {
+                    if (Vt_visual_scroll_down(vt)) {
+                        break;
+                    }
                 }
             }
-            if (self->vt.selection.mode) {
-                Vt_select_set_end_cell(&self->vt, self->ksm_cursor.col, self->ksm_cursor.row);
-            }
+            L_UPDATE_SELECT_END
             break;
 
-        case 65362: // up
-        case 107:   // k
+        case KEY(Up):
+        case KEY(k):
             App_notify_content_change(self);
             if (self->ksm_cursor.row) {
                 self->ksm_cursor.row--;
             }
-            while (Vt_visual_top_line(&self->vt) > self->ksm_cursor.row) {
-                Vt_visual_scroll_up(&self->vt);
+            while (Vt_visual_top_line(vt) > self->ksm_cursor.row) {
+                if (Vt_visual_scroll_up(vt)) {
+                    break;
+                }
             }
-            if (self->vt.selection.mode) {
-                Vt_select_set_end_cell(&self->vt, self->ksm_cursor.col, self->ksm_cursor.row);
-            }
+            L_UPDATE_SELECT_END
             break;
 
-        case 65363: // right
-        case 108:   // l
+        case KEY(Right):
+        case KEY(l):
             App_notify_content_change(self);
-            if (self->ksm_cursor.col + 1 < self->vt.ws.ws_col) {
+            if (self->ksm_cursor.col + 1 < Vt_col(vt)) {
                 self->ksm_cursor.col++;
             }
-            if (self->vt.selection.mode) {
-                Vt_select_set_end_cell(&self->vt, self->ksm_cursor.col, self->ksm_cursor.row);
-            }
+            L_UPDATE_SELECT_END
             break;
 
-        case 121: // y
-        case 99:  // c
-        {
+        case KEY(y):
+        case KEY(c): {
             if (self->vt.selection.mode) {
-                Vector_char txt = Vt_select_region_to_string(&self->vt);
+                Vector_char txt = Vt_select_region_to_string(vt);
                 if (txt.size) {
                     App_clipboard_send(self, txt.buf);
-                    Vt_select_end(&self->vt);
+                    Vt_select_end(vt);
                 } else {
                     Vector_destroy_char(&txt);
                 }
@@ -442,78 +450,80 @@ static bool App_handle_keyboard_select_mode_key(App*     self,
 
         case 13: // Return
             if (self->vt.selection.mode) {
-                Vt_select_end(&self->vt);
+                Vt_select_end(vt);
                 break;
             }
-            Vt_select_init_cell(&self->vt,
-                                FLAG_IS_SET(mods, MODIFIER_CONTROL) ? SELECT_MODE_BOX
-                                                                    : SELECT_MODE_NORMAL,
-                                self->ksm_cursor.col,
-                                self->ksm_cursor.row);
-            Vt_select_commit(&self->vt);
+            size_t          col = self->ksm_cursor.col;
+            size_t          row = (self->ksm_cursor.row) - Vt_visual_top_line(vt);
+            enum SelectMode mode =
+              FLAG_IS_SET(mods, MODIFIER_CONTROL) ? SELECT_MODE_BOX : SELECT_MODE_NORMAL;
+            Vt_select_init_cell(vt, mode, col, row);
+            Vt_select_commit(vt);
             break;
 
-        case 98: { // b
+        case KEY(b): {
             // jump back by word
             for (bool initial = true;; initial = false) {
                 if (!self->ksm_cursor.col) {
                     break;
                 }
-                char32_t code =
-                  self->vt.lines.buf[self->ksm_cursor.row].data.buf[self->ksm_cursor.col].rune.code;
-                char32_t prev_code = self->vt.lines.buf[self->ksm_cursor.row]
-                                       .data.buf[self->ksm_cursor.col - 1]
-                                       .rune.code;
+                VtRune* rune      = Vt_at(vt, self->ksm_cursor.col, self->ksm_cursor.row);
+                VtRune* prev_rune = Vt_at(vt, self->ksm_cursor.col - 1, self->ksm_cursor.row);
+                if (!rune || !prev_rune) {
+                    break;
+                }
+                char32_t code = rune->rune.code, prev_code = prev_rune->rune.code;
+
                 if (isblank(prev_code) && !isblank(code) && !initial) {
                     break;
                 }
                 --self->ksm_cursor.col;
                 App_notify_content_change(self);
             }
-            Vt_select_set_end_cell(&self->vt, self->ksm_cursor.col, self->ksm_cursor.row);
+            L_UPDATE_SELECT_END
         } break;
 
-        case 119: { // w
+        case KEY(w): {
             // jump forward to next word
             for (bool initial = true;; initial = false) {
-                if (self->ksm_cursor.col + 1 >= self->vt.ws.ws_col ||
-                    self->ksm_cursor.col + 1 >=
-                      self->vt.lines.buf[self->ksm_cursor.row].data.size) {
+                VtLine* row_line = Vt_line_at(vt, self->ksm_cursor.row);
+                if (self->ksm_cursor.col + 1 >= Vt_col(vt) || !row_line ||
+                    self->ksm_cursor.col + 1 >= row_line->data.size) {
                     break;
                 }
-                char32_t code =
-                  self->vt.lines.buf[self->ksm_cursor.row].data.buf[self->ksm_cursor.col].rune.code;
-                char32_t next_code = self->vt.lines.buf[self->ksm_cursor.row]
-                                       .data.buf[self->ksm_cursor.col + 1]
-                                       .rune.code;
+                VtRune* rune      = Vt_at(vt, self->ksm_cursor.col, self->ksm_cursor.row);
+                VtRune* next_rune = Vt_at(vt, self->ksm_cursor.col + 1, self->ksm_cursor.row);
+                if (!rune || !next_rune) {
+                    break;
+                }
+                char32_t code = rune->rune.code, next_code = next_rune->rune.code;
                 ++self->ksm_cursor.col;
                 App_notify_content_change(self);
-                if ((isblank(code) && !isblank(next_code)) && !initial)
+                if ((isblank(code) && !isblank(next_code)) && !initial) {
                     break;
+                }
             }
-            Vt_select_set_end_cell(&self->vt, self->ksm_cursor.col, self->ksm_cursor.row);
+            L_UPDATE_SELECT_END
         } break;
 
-        case 101: { // e
+        case KEY(e): {
             // jump to end of word
             for (bool initial = true;; initial = false) {
-                if (self->ksm_cursor.col + 1 >= self->vt.ws.ws_col ||
-                    self->ksm_cursor.col + 1 >=
-                      self->vt.lines.buf[self->ksm_cursor.row].data.size) {
+                VtLine* row_line = Vt_line_at(vt, self->ksm_cursor.row);
+                if (self->ksm_cursor.col + 1 >= Vt_col(vt) || !row_line ||
+                    self->ksm_cursor.col + 1 >= row_line->data.size) {
                     break;
                 }
-                char32_t code = self->vt.lines.buf[self->ksm_cursor.row]
-                                  .data.buf[self->ksm_cursor.col]
-                                  .rune.code,
-                         next_code = self->vt.lines.buf[self->ksm_cursor.row]
-                                       .data.buf[self->ksm_cursor.col + 1]
-                                       .rune.code;
-                if ((isblank(next_code) && !isblank(code)) && !initial)
+                VtRune*  rune      = Vt_at(vt, self->ksm_cursor.col, self->ksm_cursor.row);
+                VtRune*  next_rune = Vt_at(vt, self->ksm_cursor.col + 1, self->ksm_cursor.row);
+                char32_t code = rune->rune.code, next_code = next_rune->rune.code;
+                if ((isblank(next_code) && !isblank(code)) && !initial) {
                     break;
+                }
                 ++self->ksm_cursor.col;
                 App_notify_content_change(self);
             }
-            Vt_select_set_end_cell(&self->vt, self->ksm_cursor.col, self->ksm_cursor.row);
+            L_UPDATE_SELECT_END
         } break;
 
         default:
@@ -530,12 +540,18 @@ static bool App_maybe_handle_application_key(App*     self,
                                              uint32_t rawkey,
                                              uint32_t mods)
 {
-    Vt* vt = &self->vt;
+    Vt*         vt  = &self->vt;
+    KeyCommand* cmd = settings.key_commands;
+
     if (self->keyboard_select_mode) {
-        self->keyboard_select_mode = !App_handle_keyboard_select_mode_key(self, key, rawkey, mods);
+        if (!(self->keyboard_select_mode =
+                !App_handle_keyboard_select_mode_key(self, key, rawkey, mods))) {
+            App_notify_content_change(self);
+        }
         return true;
     }
-    if (KeyCommand_is_active(&settings.key_commands[KCMD_COPY], key, rawkey, mods)) {
+
+    if (KeyCommand_is_active(&cmd[KCMD_COPY], key, rawkey, mods)) {
         if (vt->selection.mode) {
             Vector_char txt = Vt_select_region_to_string(vt);
             if (txt.size)
@@ -544,10 +560,10 @@ static bool App_maybe_handle_application_key(App*     self,
                 Vector_destroy_char(&txt);
         }
         return true;
-    } else if (KeyCommand_is_active(&settings.key_commands[KCMD_PASTE], key, rawkey, mods)) {
+    } else if (KeyCommand_is_active(&cmd[KCMD_PASTE], key, rawkey, mods)) {
         App_clipboard_get(self);
         return true;
-    } else if (KeyCommand_is_active(&settings.key_commands[KCMD_FONT_SHRINK], key, rawkey, mods)) {
+    } else if (KeyCommand_is_active(&cmd[KCMD_FONT_SHRINK], key, rawkey, mods)) {
         if (settings.font_size > 1) {
             --settings.font_size;
             Vt_clear_all_proxies(vt);
@@ -559,7 +575,7 @@ static bool App_maybe_handle_application_key(App*     self,
             App_flash(self);
         }
         return true;
-    } else if (KeyCommand_is_active(&settings.key_commands[KCMD_FONT_ENLARGE], key, rawkey, mods)) {
+    } else if (KeyCommand_is_active(&cmd[KCMD_FONT_ENLARGE], key, rawkey, mods)) {
         ++settings.font_size;
         Vt_clear_all_proxies(vt);
         App_reload_font(self);
@@ -567,25 +583,42 @@ static bool App_maybe_handle_application_key(App*     self,
         Vt_resize(vt, cells.first, cells.second);
         App_notify_content_change(self);
         return true;
-    } else if (KeyCommand_is_active(&settings.key_commands[KCMD_DEBUG], key, rawkey, mods)) {
+    } else if (KeyCommand_is_active(&cmd[KCMD_DEBUG], key, rawkey, mods)) {
         Vt_dump_info(vt);
         return true;
-    } else if (KeyCommand_is_active(&settings.key_commands[KCMD_UNICODE_ENTRY],
-                                    key,
-                                    rawkey,
-                                    mods)) {
+    } else if (KeyCommand_is_active(&cmd[KCMD_UNICODE_ENTRY], key, rawkey, mods)) {
         Vt_start_unicode_input(vt);
         return true;
-    } else if (KeyCommand_is_active(&settings.key_commands[KCMD_KEYBOARD_SELECT],
-                                    key,
-                                    rawkey,
-                                    mods)) {
+    } else if (KeyCommand_is_active(&cmd[KCMD_KEYBOARD_SELECT], key, rawkey, mods)) {
         self->ksm_cursor           = self->vt.cursor;
         self->ksm_cursor.blinking  = false;
         self->ui.cursor            = &self->ksm_cursor;
         self->keyboard_select_mode = true;
+        self->ksm_cursor.row =
+          CLAMP(self->ksm_cursor.row, Vt_visual_top_line(vt), Vt_visual_bottom_line(vt));
+        App_notify_content_change(self);
         return true;
-    } else if (KeyCommand_is_active(&settings.key_commands[KCMD_DUPLICATE], key, rawkey, mods)) {
+    } else if (KeyCommand_is_active(&cmd[KCMD_LINE_SCROLL_UP], key, rawkey, mods)) {
+        Vt_visual_scroll_up(vt);
+        self->ui.scrollbar.visible = true;
+        App_notify_content_change(self);
+        return true;
+    } else if (KeyCommand_is_active(&cmd[KCMD_LINE_SCROLL_DN], key, rawkey, mods)) {
+        Vt_visual_scroll_down(vt);
+        self->ui.scrollbar.visible = true;
+        App_notify_content_change(self);
+        return true;
+    } else if (KeyCommand_is_active(&cmd[KCMD_PAGE_SCROLL_UP], key, rawkey, mods)) {
+        Vt_visual_scroll_page_up(vt);
+        self->ui.scrollbar.visible = true;
+        App_notify_content_change(self);
+        return true;
+    } else if (KeyCommand_is_active(&cmd[KCMD_PAGE_SCROLL_DN], key, rawkey, mods)) {
+        Vt_visual_scroll_page_down(vt);
+        self->ui.scrollbar.visible = true;
+        App_notify_content_change(self);
+        return true;
+    } else if (KeyCommand_is_active(&cmd[KCMD_DUPLICATE], key, rawkey, mods)) {
         /* use the full path to the running file (it may have been started with a relative path) */
         char* this_file  = get_running_binary_path();
         char* old_argv_0 = settings.argv[0];
@@ -710,12 +743,9 @@ static bool App_scrollbar_consume_click(App*     self,
             } else if (state && button == MOUSE_BTN_MIDDLE) {
                 /* jump one screen in that direction */
                 if (dp > self->ui.scrollbar.top + self->ui.scrollbar.length / 2) {
-                    Vt_visual_scroll_to(vt, vt->visual_scroll_top + vt->ws.ws_row);
+                    Vt_visual_scroll_page_down(vt);
                 } else {
-                    size_t to = vt->visual_scroll_top > vt->ws.ws_row
-                                  ? vt->visual_scroll_top - vt->ws.ws_row
-                                  : 0;
-                    Vt_visual_scroll_to(vt, to);
+                    Vt_visual_scroll_page_up(vt);
                 }
             }
         }
@@ -913,11 +943,14 @@ static void App_button_handler(void*    self,
     x                    = CLAMP(x - app->ui.pixel_offset_x, 0, (int32_t)app->resolution.first);
     y                    = CLAMP(y - app->ui.pixel_offset_y, 0, (int32_t)app->resolution.second);
     bool vt_wants_scroll = Vt_reports_mouse(&app->vt);
+
     if (!vt_wants_scroll && (button == MOUSE_BTN_WHEEL_DOWN && state)) {
         uint8_t lines             = ammount ? ammount : settings.scroll_discrete_lines;
         app->ui.scrollbar.visible = true;
         for (uint8_t i = 0; i < lines; ++i) {
-            Vt_visual_scroll_down(vt);
+            if (Vt_visual_scroll_down(vt)) {
+                break;
+            }
         }
         App_update_scrollbar_dims(self);
         App_notify_content_change(self);
@@ -925,7 +958,9 @@ static void App_button_handler(void*    self,
         uint8_t lines             = ammount ? ammount : settings.scroll_discrete_lines;
         app->ui.scrollbar.visible = true;
         for (uint8_t i = 0; i < lines; ++i) {
-            Vt_visual_scroll_up(vt);
+            if (Vt_visual_scroll_up(vt)) {
+                break;
+            }
         }
         App_update_scrollbar_vis(self);
         App_update_scrollbar_dims(self);

src/gfx_gl21.c
@@ -2277,16 +2277,22 @@ __attribute__((hot)) static inline void GfxOpenGL21_rasterize_line(GfxOpenGL21* 
 
 static inline void GfxOpenGL21_draw_cursor(GfxOpenGL21* gfx, const Vt* vt, const Ui* ui)
 {
-    if ((!vt->cursor.hidden &&
-         (((ui->cursor->blinking && gfx->in_focus) ? gfx->draw_blinking
-                                                   : true || gfx->recent_action) ||
-          !settings.enable_cursor_blink))) {
+    bool show_blink =
+      !settings.enable_cursor_blink ||
+      ((ui->cursor->blinking && gfx->in_focus) ? gfx->draw_blinking : true || gfx->recent_action);
+
+    if (show_blink && !ui->cursor->hidden) {
+        bool filled_block = false;
+
         size_t row = ui->cursor->row - Vt_visual_top_line(vt), col = ui->cursor->col;
-        bool   filled_block = false;
+        if (row >= Vt_row(vt)) {
+            return;
+        }
+
         Vector_clear_GlyphBufferData(gfx->vec_glyph_buffer);
         Vector_clear_vertex_t(&gfx->vec_vertex_buffer);
 
-        switch (vt->cursor.type) {
+        switch (ui->cursor->type) {
             case CURSOR_BEAM:
                 Vector_pushv_vertex_t(
                   &gfx->vec_vertex_buffer,
@@ -2621,7 +2627,7 @@ static void GfxOpenGL21_draw_overlays(GfxOpenGL21* self, const Vt* vt, const Ui*
 {
     if (vt->unicode_input.active) {
         GfxOpenGL21_draw_unicode_input(self, vt);
-    } else if (!vt->scrolling_visual) {
+    } else {
         GfxOpenGL21_draw_cursor(self, vt, ui);
     }
     if (ui->scrollbar.visible) {

src/key.h
@@ -1,0 +1,9 @@
+#pragma once
+
+#ifdef NOWL
+#include <X11/keysym.h>
+#define KEY(name) (XK_##name)
+#else
+#include <xkbcommon/xkbcommon.h>
+#define KEY(name) (XKB_KEY_##name)
+#endif

src/opts.h
@@ -194,34 +194,46 @@ static struct option long_options[] = {
 #define OPT_BIND_KEY_UNI_IDX 56
     [OPT_BIND_KEY_UNI_IDX] = { "bind-key-unicode", required_argument, 0, 0 },
 
-#define OPT_BIND_KEY_KSM_IDX 57
+#define OPT_BIND_KEY_PG_UP_IDX 57
+    [OPT_BIND_KEY_PG_UP_IDX] = { "bind-key-pg-up", required_argument, 0, 0 },
+
+#define OPT_BIND_KEY_PG_DN_IDX 58
+    [OPT_BIND_KEY_PG_DN_IDX] = { "bind-key-pg-down", required_argument, 0, 0 },
+
+#define OPT_BIND_KEY_LN_UP_IDX 59
+    [OPT_BIND_KEY_LN_UP_IDX] = { "bind-key-ln-up", required_argument, 0, 0 },
+
+#define OPT_BIND_KEY_LN_DN_IDX 60
+    [OPT_BIND_KEY_LN_DN_IDX] = { "bind-key-ln-down", required_argument, 0, 0 },
+
+#define OPT_BIND_KEY_KSM_IDX 61
     [OPT_BIND_KEY_KSM_IDX] = { "bind-key-kbd-select", required_argument, 0, 0 },
 
-#define OPT_BIND_KEY_DUP_IDX 58
+#define OPT_BIND_KEY_DUP_IDX 62
     [OPT_BIND_KEY_DUP_IDX] = { "bind-key-duplicate", required_argument, 0, 0 },
 
-#define OPT_BIND_KEY_DEBUG_IDX 59
+#define OPT_BIND_KEY_DEBUG_IDX 63
     [OPT_BIND_KEY_DEBUG_IDX] = { "bind-key-debug", required_argument, 0, 0 },
 
-#define OPT_BIND_KEY_QUIT_IDX 60
+#define OPT_BIND_KEY_QUIT_IDX 64
     [OPT_BIND_KEY_QUIT_IDX] = { "bind-key-quit", required_argument, 0, 0 },
 
-#define OPT_DEBUG_PTY_IDX 61
+#define OPT_DEBUG_PTY_IDX 65
     [OPT_DEBUG_PTY_IDX] = { "debug-pty", no_argument, 0, 'D' },
 
-#define OPT_DEBUG_GFX_IDX 62
+#define OPT_DEBUG_GFX_IDX 66
     [OPT_DEBUG_GFX_IDX] = { "debug-gfx", no_argument, 0, 'G' },
 
-#define OPT_DEBUG_FONT_IDX 63
+#define OPT_DEBUG_FONT_IDX 67
     [OPT_DEBUG_FONT_IDX] = { "debug-font", no_argument, 0, 'F' },
 
-#define OPT_VERSION_IDX 64
+#define OPT_VERSION_IDX 68
     [OPT_VERSION_IDX] = { "version", no_argument, 0, 'v' },
 
-#define OPT_HELP_IDX 65
+#define OPT_HELP_IDX 69
     [OPT_HELP_IDX] = { "help", no_argument, 0, 'h' },
 
-#define OPT_SENTINEL_IDX 66
+#define OPT_SENTINEL_IDX 70
     [OPT_SENTINEL_IDX] = { 0 }
 };
 
@@ -267,7 +279,8 @@ static const char* long_options_descriptions[][2] = {
     [OPT_H_FG_COLOR_IDX]  = { arg_color, "Highlighted text foreground color" },
     [OPT_NO_FLASH_IDX]    = { NULL, "Disable visual bell" },
     [OPT_COLORSCHEME_IDX] = { "name/int",
-                              "Colorscheme name/index: wayst, linux, xterm, rxvt, yaru, tango, orchis, "
+                              "Colorscheme name/index: wayst, linux, xterm, rxvt, yaru, tango, "
+                              "orchis, "
                               "solarized" },
 
     [OPT_FONT_IDX]               = { "[font, ...]", "Primary fonts (default: Monospace)" },
@@ -305,6 +318,13 @@ static const char* long_options_descriptions[][2] = {
     [OPT_BIND_KEY_SHRINK_IDX]  = { arg_key, "Shrink font key command (default: C+S+minus)" },
     [OPT_BIND_KEY_UNI_IDX]     = { arg_key,
                                "Unicode entry mode activation key command (default: C+S+u)" },
+
+    [OPT_BIND_KEY_LN_UP_IDX] = { arg_key, "Scroll up by line key command (default: C+S+Up)" },
+    [OPT_BIND_KEY_LN_DN_IDX] = { arg_key, "Scroll down by line key command (default: C+S+Down)" },
+    [OPT_BIND_KEY_PG_UP_IDX] = { arg_key, "Scroll up by page key command (default: C+S+Page_Up)" },
+    [OPT_BIND_KEY_PG_DN_IDX] = { arg_key,
+                                 "Scroll down by page key command (default: C+S+Page_Down)" },
+
     [OPT_BIND_KEY_KSM_IDX] = { arg_key, "Enter keyboard select mode key command (default: C+S+k)" },
     [OPT_BIND_KEY_DUP_IDX] = { arg_key,
                                "Duplicate key command (start new instance in work directory set by "

src/settings.c
@@ -13,6 +13,7 @@
 #include <fontconfig/fontconfig.h>
 
 #include "fontconfig.h"
+#include "key.h"
 #include "opts.h"
 #include "settings.h"
 #include "util.h"
@@ -402,43 +403,75 @@ static void settings_make_default()
                 .is_name = false,
                 .mods = 0,
             },
+
             [KCMD_UNICODE_ENTRY] = (KeyCommand) {
-                .key.code = 117, // u
+                .key.code = KEY(u),
                 .is_name = false,
                 .mods = MODIFIER_SHIFT | MODIFIER_CONTROL
             },
+
             [KCMD_COPY] = (KeyCommand) {
-                .key.code = 99, // c
+                .key.code = KEY(c),
                 .is_name = false,
                 .mods = MODIFIER_SHIFT | MODIFIER_CONTROL
             },
+
             [KCMD_PASTE] = (KeyCommand) {
-                .key.code = 118, // v
+                .key.code = KEY(v),
                 .is_name = false,
                 .mods = MODIFIER_SHIFT | MODIFIER_CONTROL
             },
+
             [KCMD_FONT_ENLARGE] = (KeyCommand) {
-                .key.code = 61, // equal
+                .key.code = KEY(equal),
                 .is_name = false,
                 .mods = MODIFIER_SHIFT | MODIFIER_CONTROL
             },
+
             [KCMD_FONT_SHRINK] = (KeyCommand) {
-                .key.code = 45, // minus
+                .key.code = KEY(minus),
                 .is_name = false,
                 .mods = MODIFIER_SHIFT | MODIFIER_CONTROL
             },
+
+            [KCMD_LINE_SCROLL_UP] = (KeyCommand) {
+                .key.code = KEY(Up),
+                .is_name = false,
+                .mods = MODIFIER_SHIFT | MODIFIER_CONTROL
+            },
+
+            [KCMD_LINE_SCROLL_DN] = (KeyCommand) {
+                .key.code = KEY(Down),
+                .is_name = false,
+                .mods = MODIFIER_SHIFT | MODIFIER_CONTROL
+            },
+
+            [KCMD_PAGE_SCROLL_UP] = (KeyCommand) {
+                .key.code = KEY(Page_Up),
+                .is_name = false,
+                .mods = MODIFIER_SHIFT | MODIFIER_CONTROL
+            },
+
+            [KCMD_PAGE_SCROLL_DN] = (KeyCommand) {
+                .key.code = KEY(Page_Down),
+                .is_name = false,
+                .mods = MODIFIER_SHIFT | MODIFIER_CONTROL
+            },
+
             [KCMD_KEYBOARD_SELECT] = (KeyCommand) {
-                .key.code = 107, // k
+                .key.code = KEY(k),
                 .is_name = false,
                 .mods = MODIFIER_SHIFT | MODIFIER_CONTROL
             },
+
             [KCMD_DUPLICATE] = (KeyCommand) {
-                .key.code = 100, // d
+                .key.code = KEY(d),
                 .is_name = false,
                 .mods = MODIFIER_SHIFT | MODIFIER_CONTROL
             },
+
             [KCMD_DEBUG] = (KeyCommand) {
-                .key.code = 47, // ?
+                .key.code = KEY(slash),
                 .is_name = false,
                 .mods = MODIFIER_SHIFT | MODIFIER_CONTROL
             },
@@ -1078,14 +1111,7 @@ static void handle_option(const char opt, const int array_index, const char* val
             }
         } break;
 
-        case OPT_BIND_KEY_COPY_IDX:
-        case OPT_BIND_KEY_PASTE_IDX:
-        case OPT_BIND_KEY_ENLARGE_IDX:
-        case OPT_BIND_KEY_SHRINK_IDX:
-        case OPT_BIND_KEY_UNI_IDX:
-        case OPT_BIND_KEY_KSM_IDX:
-        case OPT_BIND_KEY_DEBUG_IDX:
-        case OPT_BIND_KEY_QUIT_IDX: {
+        case OPT_BIND_KEY_COPY_IDX ... OPT_BIND_KEY_QUIT_IDX: {
             // point 'name_start' to after the last '+'
             const char* name_start = value;
             while (*name_start)
@@ -1138,8 +1164,23 @@ static void handle_option(const char opt, const int array_index, const char* val
                 case OPT_BIND_KEY_UNI_IDX:
                     command = &settings.key_commands[KCMD_UNICODE_ENTRY];
                     break;
+                case OPT_BIND_KEY_LN_UP_IDX:
+                    command = &settings.key_commands[KCMD_LINE_SCROLL_UP];
+                    break;
+                case OPT_BIND_KEY_LN_DN_IDX:
+                    command = &settings.key_commands[KCMD_LINE_SCROLL_DN];
+                    break;
+                case OPT_BIND_KEY_PG_UP_IDX:
+                    command = &settings.key_commands[KCMD_PAGE_SCROLL_UP];
+                    break;
+                case OPT_BIND_KEY_PG_DN_IDX:
+                    command = &settings.key_commands[KCMD_PAGE_SCROLL_DN];
+                    break;
                 case OPT_BIND_KEY_KSM_IDX:
                     command = &settings.key_commands[KCMD_KEYBOARD_SELECT];
+                    break;
+                case OPT_BIND_KEY_DUP_IDX:
+                    command = &settings.key_commands[KCMD_DUPLICATE];
                     break;
                 case OPT_BIND_KEY_DEBUG_IDX:
                     command = &settings.key_commands[KCMD_DEBUG];

src/settings.h
@@ -102,6 +102,10 @@ enum KeyCommands
     KCMD_FONT_ENLARGE,
     KCMD_FONT_SHRINK,
     KCMD_UNICODE_ENTRY,
+    KCMD_LINE_SCROLL_UP,
+    KCMD_LINE_SCROLL_DN,
+    KCMD_PAGE_SCROLL_UP,
+    KCMD_PAGE_SCROLL_DN,
     KCMD_KEYBOARD_SELECT,
     KCMD_DUPLICATE,
     KCMD_DEBUG,

commit f7301de23ccea4bb56c3b5ec72820571b937feb0

Fix timing issue introduced by 7d5d676919f78e29292872420c9eafa118ad2e9e (fixes #64)

src/main.c
@@ -191,7 +191,7 @@ static void App_run(App* self)
         int timeout_ms = 0;
         if (!self->swap_performed) {
             if (self->closest_pending_wakeup) {
-                timeout_ms = MAX(0, TimePoint_is_ms_ahead(*(self->closest_pending_wakeup)));
+                timeout_ms = TimePoint_is_ms_ahead(*(self->closest_pending_wakeup));
             } else {
                 timeout_ms = -1;
             }